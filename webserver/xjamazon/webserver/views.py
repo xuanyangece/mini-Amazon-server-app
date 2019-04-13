e066e171a117bcb1104d50650b95d5a3899f81f8
Xuan Yang <xy91@duke.edu>
2019-04-13 13:14:12 -0400

    Ready to add button for buy

--- webserver/xjamazon/webserver/views.py
+++ webserver/xjamazon/webserver/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+import socket
+
+HOST, PORT = "vcm-8965.vm.duke.edu", 65432
 
 def index(request):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    app_server_ip = socket.gethostbyname(HOST)
+    s.connect((app_server_ip, PORT))
+    s.sendall('wo cao ni ma'.encode('utf-8'))
     return HttpResponse("Hello world.")

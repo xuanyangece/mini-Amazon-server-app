f40dbd10d5894cd99fc6dd2df40b365eaaf7e557
Xuan Yang <xy91@duke.edu>
2019-04-19 17:08:41 -0400

    change helper2.py to fit new truck->packageid map

--- webserver/helper2.py
+++ webserver/helper2.py
@@ -1,5 +1,21 @@
+import xml.dom.minidom as minidom
+import xml.etree.ElementTree as ET
 import xml.sax
+import socket
+import select
 
+WorldMessage = ["MMP"]
+
+UPSMessage = ["RNM"]
+
+ship_truckMap = dict() # map from shipid to PackOnTruck
+
+truck_packageMap = dict() # map from truck id to package list
+# (3) -> (4, 1, 2)
+
+UPSHOST, UPSPORT = "vcm-xxxx.vm.duke.edu", 41414
+
+# parse reqTruck
 class TruckHandler(xml.sax.ContentHandler):
     def __init__(self):
         self.CurrentData = ""
@@ -25,18 +41,159 @@ class TruckHandler(xml.sax.ContentHandler):
             self.packageID.append(attributes["id"])
             print ("package id: ", attributes["id"])
 
-Handler = TruckHandler()
 
-st = """
-<reqTruck>
-   <Order id="13"/>
-   <Truck id="54"/>
-   <Packages>
-	<package id="9" TrackingNumber="123"/>
-    <package id="4" TrackingNumber="666"/>	    
-   </Packages>
-</reqTruck>
-"""
+# helper function for prettify XML from https://stackoverflow.com/questions/17402323/use-xml-etree-elementtree-to-print-nicely-formatted-xml-files
+def prettify(elem):
+    """Return a pretty-printed XML string for the Element.
+    """
+    rough_string = ET.tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="\t")
 
-xml.sax.parseString(st, Handler)
-print(len(Handler.packageID))
+
+def goDeliverXML(truckID):
+    str = "<goDeliver>\n\t<Truck id=\""
+    str += truckID + "\"/>\n"
+    str += "</goDeliver>\n"
+
+    return str.encode('utf-8')
+
+# receive from UPS
+def recvUPS(s, timeout):
+    ready = select.select([s], [], [], timeout)
+    if ready[0]:
+        # pop out UPSMessage
+        UPSMessage.pop(0)
+        # receive result
+        data = s.recv(10240)
+        # parse data and do something
+        data = str(data)
+        print("*******From UPS*******\n")
+        print(data)
+
+        if data.find("goLoad") != -1:
+            # parse get truck id
+            Handler = TruckHandler() # only use Truck id in it
+            xml.sax.parseString(data, Handler)
+            tid = Handler.truckID
+
+            # traverse all packageids
+            pkgids = truck_packageMap[tid]
+
+            for pkgid in pkgids:
+                # get the pot
+                pot = ship_truckMap[pkgid]
+                for x in pot.load:
+                    # pot update
+                    x.truckid = tid
+                
+                # put in in WorldMessage
+                WorldMessage.append(pot)
+
+                # remove pot
+                ship_truckMap.pop(pkgid)
+
+        elif data.find("reqTruck") != -1:
+            # parse
+            Handler = TruckHandler()
+            xml.sax.parseString(data, Handler)
+
+            # map from truck id to multiple packageid
+            tid = Handler.truckID
+            pkgids = Handler.packageID
+
+            # check exist
+            if tid not in truck_packageMap.keys():
+                truck_packageMap[tid] = []
+
+            # update pot for each pkgid
+            for pkgid in pkgids:
+                truck_packageMap[tid].append(pkgid)
+
+
+def handleUPS():
+    global UPSHOST
+    global UPSPORT
+    # poll to check whether there's remaining commands in UPSMessage
+    while True:
+        global UPSMessage
+        # not empty: handle!
+        if (len(UPSMessage) != 0):
+            # extra info from UPSMessage
+            message = UPSMessage[0]
+
+            # send UPSMessage info to UPS
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            app_server_ip = socket.gethostbyname(UPSHOST)
+            s.connect((app_server_ip, UPSPORT))
+            s.sendall(message.encode('utf-8'))
+
+            # recv, ACK/handle and add
+            s.setblocking(0)
+            timeout = 60 * 1
+            recvUPS(s, timeout)
+            
+        else:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            app_server_ip = socket.gethostbyname(UPSHOST)
+            s.connect((app_server_ip, UPSPORT))
+            
+            # recv, ACK/handle and add
+            s.setblocking(0)
+            timeout = 20 * 1
+            recvUPS(s, timeout)
+
+
+
+
+class Item:
+    def __init__(self, name, quantity, description):
+        self.name = str(name)
+        self.quantity = str(quantity)
+        self.description = str(description)
+
+class Package:
+    def __init__(self, id, x, y, upsname, items):
+        self.id = str(id)
+        self.x = str(x)
+        self.y = str(y)
+        self.upsname = str(upsname)
+        self.items = items
+
+# packages = [p1, p2, p3, ...]
+def reqTruckXML(orderID, whID, packages):
+    strXML = "<reqTruck>\n\t"
+    strXML += "<Order id=\"" + str(orderID) + "\"/>\n\t"
+    strXML += "<Warehouse id=\"" + str(whID) + "\"/>\n\t"
+    strXML += "<Packages>\n"
+
+    # traverse packages
+    for pkg in packages:
+        strXML += "\t\t<Package id=\"" + pkg.id + "\">\n"
+
+        strXML += "\t\t\t<destination X=\"" + pkg.x + "\" Y=\"" + pkg.y + "\"/>\n" 
+        strXML += "\t\t\t<UPS username=\"" + pkg.upsname + "\"/>\n"
+        strXML += "\t\t\t<items>\n"
+
+        for item in pkg.items:
+            strXML += "\t\t\t\t<item name=\"" + item.name + "\" quantity=\"" + item.quantity + "\" description=\""
+            strXML += item.description + "\"/>\n"
+
+        strXML += "\t\t<Package>\n"
+
+    strXML += "\t</Packages>\n"
+    strXML += "</reqTruck>\n"
+
+    return strXML
+
+
+items1 = []
+items1.append(Item("1", 10, "AI"))
+items1.append(Item("2", 5, "ML"))
+
+packages1 = []
+packages1.append(Package(1, 3, 5, "myUPS", items1))
+
+#print(goDeliverXML("1314"))
+
+print(reqTruckXML(99, 88, packages1))

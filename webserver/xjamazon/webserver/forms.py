1dcc858c3a7f07cbdd28d2a0dda35ecb83e65f6d
Xuan Yang <xy91@duke.edu>
2019-04-22 14:25:22 -0400

    Choice for user from items

--- webserver/xjamazon/webserver/forms.py
+++ webserver/xjamazon/webserver/forms.py
@@ -1,20 +1,27 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
+from .models import Product
 import re
 
 
 class BuyProductForm(forms.Form):
-    item_id = forms.CharField(label='Item ID', max_length=20)
     ups_name = forms.CharField(label='UPS name', max_length=30)
-    description = forms.CharField(label='Description', max_length=200)
     count = forms.IntegerField(label='Count', validators=[MaxValueValidator(999999),MinValueValidator(1)])
     x = forms.IntegerField(label='x')
     y = forms.IntegerField(label='y')
 
+    def __init__(self, *args, **kwargs):
+        super(BuyProductForm, self).__init__(*args, **kwargs)
+        mychoices = Product.objects.all()
+        self.fields['item_id'] = forms.ChoiceField(
+            label='Item ID',
+            choices=[(o.item_id, str(o.item_id)) for o in mychoices]
+        )
+
 
 class WarehouseForm(forms.Form):
-    x = forms.IntegerField(label='x')
+    x = forms.IntegerField(label='Number of warehouse you want to create')
 
 
 class LoginForm(forms.Form):
